c515be57a0458d57f5615b03294db73aae2c0e09
mattersonline <matte@10.204.181.147>
2017-05-02 00:38:41 -0500

    added black background

--- HCI Media Viewer/src/application/Main.java
+++ HCI Media Viewer/src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
 	
